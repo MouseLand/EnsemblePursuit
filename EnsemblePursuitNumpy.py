9442e2a8b601fd6996aba1d88e3a3cf88a91238a
mariakesa <maria.kesa@gmail.com>
2019-06-10 07:21:35 +0300

    Add functionality for sorting C.

--- EnsemblePursuitNumpy.py
+++ EnsemblePursuitNumpy.py
@@ -14,7 +14,31 @@ class EnsemblePursuitNumpy():
         X=np.subtract(X,mean_stimuli)
         X=np.divide(X,std_stimuli)
         return X
+
+    def fit_one_ensemble(self,X):
+        C=X@X.T
+        #A parameter to account for how many top neurons we sample from. It starts from 1,
+        #because we choose the top neuron when possible, e.g. when we can find an ensemble
+        # that is larger than min ensemble size. If there is no ensemble with the top neuron
+        # we increase the number of neurons to sample from.
+        self.n_neurons_for_sampling=1
+        top_neurons=self.sorting_for_seed(C)
+
+    def sorting_for_seed(self,C):
+        '''
+        This function sorts the similarity matrix C to find neurons that are most correlated
+        to their nr_neurons_to_av neighbors (we average over the neighbors).
+        '''
+        nr_neurons_to_av=self.options_dict['seed_neuron_av_nr']
+        sorted_similarities=np.sort(C,axis=1)[:,:-1][:,self.sz[0]-nr_neurons_to_av-1:] 
+        print(sorted_similarities.shape)
+        average_similarities=np.mean(sorted_similarities,axis=1)
+        top_neurons=np.argsort(average_similarities)
+        print('top neurons', top_neurons)
+        return top_neurons
    
     def fit_transform(self,X):
         X=self.zscore(X)
-        print(X)
+        self.sz=X.shape
+        #print(X)
+        self.fit_one_ensemble(X)
--- EnsemblePursuitPyTorch.py
+++ EnsemblePursuitPyTorch.py
@@ -16,13 +16,38 @@ class EnsemblePursuitPyTorch():
         X=X.div(std_stimuli)
         return X.t()
 
+    def fit_one_ensemble(self,X):
+        C=X@X.t()
+        #A parameter to account for how many top neurons we sample from. It starts from 1,
+        #because we choose the top neuron when possible, e.g. when we can find an ensemble
+        #that is larger than min ensemble size. If there is no ensemble with the top neuron
+        #we increase the number of neurons to sample from.
+        self.n_neurons_for_sampling=1
+        top_neurons=self.sorting_for_seed(C)
+
+    def sorting_for_seed(self,C):
+        '''
+        This function sorts the similarity matrix C to find neurons that are most correlated
+        to their nr_neurons_to_av neighbors (we average over the neighbors).
+        '''
+        nr_neurons_to_av=self.options_dict['seed_neuron_av_nr']
+        sorted_similarities,_=C.sort(dim=1)
+        sorted_similarities=sorted_similarities[:,:-1][:,self.sz[0]-nr_neurons_to_av-1:]
+        print(sorted_similarities.size())
+        average_similarities=sorted_similarities.mean(dim=1)
+        top_neurons=average_similarities.argsort()
+        print('top neurons', top_neurons)
+        return top_neurons
+
     def fit_transform(self,X):
         '''
-             X-- shape (neurons, timepoints)
+        X-- shape (neurons, timepoints)
         '''
         X=torch.cuda.FloatTensor(X)
         X=self.zscore(X)
-        print(X)
+        #print(X)
+        self.sz=X.size()
+        self.fit_one_ensemble(X)
         
 
  
