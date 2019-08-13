2eb9fb84c6035101843540ce7dd8a0fdb3afd0bb
mariakesa <maria.kesa@gmail.com>
2019-08-13 22:15:31 +0300

    Plotting inside a window

--- SimulationGUI_10-08-2019/gui.py
+++ SimulationGUI_10-08-2019/gui.py
@@ -13,21 +13,29 @@ class MainW(QtGui.QMainWindow):
         self.setWindowTitle("EnsemblePursuit")
 
     def set_layout(self):
+        self.l0 = QtGui.QGridLayout()
         self.win = pg.GraphicsLayoutWidget()
         self.win.resize(1000,600)
+        self.l0.addWidget(self.win, 0, 0, 50, 30)
+
 
     def plot_U(self):
+        self.view=pg.GraphicsView()
         path='/home/maria/Documents/EnsemblePursuit/SAND9/experiments/natimg2800_M170717_MP034_2017-09-11.mat_U_ep_pytorch.npy'
         data=np.load(path)[:,0]
-        plot_u = self.win.addPlot(title="U",col=0,row=1)
+        self.plot_u = self.win.addPlot(row=1, col=0, rowspan=2, colspan=1, lockAspect=True)
         print(data)
-        print(plot_u)
-        plot_u.plot(data)
-        self.win.show()
+        print(self.plot_u)
+        self.plot_u.plot(data)
+        #self.win.scene()
+        #pg.show()
+        self.view.setCentralItem(self.plot_u)
+        self.setCentralWidget(self.view)
+        self.show()
+        #self.win.show()
 
 app=QApplication(sys.argv)
 window=MainW()
 window.set_layout()
 window.plot_U()
-window.show()
 sys.exit(app.exec_())
