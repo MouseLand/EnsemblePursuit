dca6e432f5e9d843ffbb0c0a58c8e523ae906f56
mariakesa <maria.kesa@gmail.com>
2019-08-31 01:15:58 +0300

    initial commit for gamma simulations

---
+++ Simulations/GammaSimulations.py
@@ -0,0 +1,47 @@
+import numpy as np
+import sys
+sys.path.append("..")
+from EnsemblePursuitModule.EnsemblePursuitPyTorch import EnsemblePursuitPyTorch
+from EnsemblePursuitModule.EnsemblePursuitNumpy import EnsemblePursuitNumpy
+
+class GammaSimulations():
+    def zscore(self,X):
+        X=X.T
+        mean_stimuli=np.mean(X,axis=0)
+        std_stimuli=np.std(X,axis=0,ddof=1)+1e-10
+        X=np.subtract(X,mean_stimuli)
+        X=np.divide(X,std_stimuli)
+        return X.T
+
+    def simulate_data(self,nr_components,nr_timepoints,nr_neurons):
+        zeros_for_U=np.random.choice([0,1], nr_neurons*nr_components, p=[1-0.01, 0.01]).reshape((nr_neurons,nr_components))
+        U=np.random.normal(loc=2,scale=1,size=(nr_neurons,nr_components))
+        U=np.abs(U*zeros_for_U)
+        V=np.random.normal(loc=0,scale=1,size=(nr_components,nr_timepoints))
+        X=U@V
+        X=self.zscore(X)
+        self.U_orig=U
+        self.V_orig=V
+        plt.hist(np.sum(zeros_for_U,axis=0))
+        plt.show()
+        #print('Mean',X.shape,np.mean(X,axis=1))
+        return X
+
+    def simulate_data_w_noise(self,nr_components,nr_timepoints,nr_neurons, noise_ampl_mult):
+        zeros_for_U=np.random.choice([0,1], nr_neurons*nr_components, p=[1-0.01, 0.01]).reshape((nr_neurons,nr_components))
+        U=np.random.normal(loc=2,scale=1,size=(nr_neurons,nr_components))
+        U=np.abs(U*zeros_for_U)
+        V=np.random.normal(loc=1,scale=1,size=(nr_components,nr_timepoints))
+        X=U@V
+        low_rank_std=np.std(X,axis=1)
+        X_noisy=np.zeros((nr_neurons,nr_timepoints))
+        for neuron in range(0,X.shape[0]):
+            noise=np.random.normal(loc=0,scale=noise_ampl_mult*low_rank_std[neuron],size=(1,nr_timepoints))
+            X_noisy[neuron,:]=X[neuron,:]+noise
+        X=self.zscore(X_noisy)
+        self.U_orig=U
+        self.V_orig=V
+        plt.hist(np.sum(zeros_for_U,axis=0))
+        plt.show()
+        #print('Mean',X.shape,np.mean(X,axis=1))
+        return X
